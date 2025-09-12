a40bd4e15db5b41eaa646bc78c065fafaab0ce35
GenicZ <121470168+Geniccc@users.noreply.github.com>
2025-09-12 10:51:20 +0700

    commit fix strapi

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,11 @@ const nextConfig: NextConfig = {
     domains: ["localhost"],
     remotePatterns: [
       // contoh: Strapi di https://api.siwisdem.id
-      { protocol: "https", hostname: "api.siwisdem.id" },
+      // { protocol: "https", hostname: "api.siwisdem.id" },
       { protocol: "https", hostname: "respected-desk-20258e8518.strapiapp.com" },
       // jika masih lokal dan diakses dari browser lain:
-      { protocol: "http", hostname: "localhost" },
-      { protocol: "http", hostname: "192.168.1.**" }, // opsional untuk LAN
+      // { protocol: "http", hostname: "localhost" },
+      // { protocol: "http", hostname: "192.168.1.**" },
     ],
   },
 };
@@ -19,6 +19,6 @@ export default nextConfig;
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    remotePatterns: [{ protocol: 'http', hostname: 'localhost', port: '1337', pathname: '/uploads/**' }],
+    remotePatterns: [{ protocol: 'https', hostname: 'respected-desk-20258e8518.strapiapp.com', pathname: '/uploads/**' }],
   },
 };
